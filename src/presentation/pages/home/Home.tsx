b53b7e2a04a92f5679fb7de4300b5bc8d7c75681
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-10-24 15:41:04 -0300

    temp: add fix request at home

--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import RemoteChooseMaster from '../../../data/usecases/RemoteChooseMaster';
+import AxiosHttpClient from '../../../infra/http/AxiosHttpClient';
+import Masters from '../../../domain/data/Masters.json';
 import Style from './Home.module.scss';
 
 function Home() {
+  async function handleStart() {
+    const httpClient = new AxiosHttpClient();
+    const response = await new RemoteChooseMaster(
+      httpClient,
+      'https://swapi.dev/api/people'
+    ).choose(Masters);
+    console.log(response);
+  }
+
   return (
     <div className={Style.home}>
       <div className={Style.welcomeWrap}>
@@ -11,7 +23,7 @@ function Home() {
         <span className={Style.challenge}>Frontend challenge</span>
       </div>
       <div className={Style.buttonWrap}>
-        <button className={Style.button} type="button">
+        <button onClick={handleStart} className={Style.button} type="button">
           START
         </button>
       </div>
