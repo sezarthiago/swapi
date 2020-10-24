cf1e76a9bb56505d36742d5a28f3d4d6cc2f05d7
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-10-24 17:55:07 -0300

    feat: home page logic

--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Masters from '../../../domain/data/Masters.json';
 import Style from './Home.module.scss';
 import { ChooseMaster } from '../../../domain/usecases/ChooseMaster';
+import { dashString } from '../../../utils/StringUtils';
 
 type Props = {
   chooseMaster: ChooseMaster;
 };
 
 function Home({ chooseMaster }: Props) {
+  const [loading, setLoading] = useState<boolean>(false);
   const history = useHistory();
 
   async function handleStart() {
-    const response = await chooseMaster.choose(Masters);
-    history.push('/master');
+    setLoading(true);
+    let master;
+    try {
+      master = await chooseMaster.choose(Masters);
+      history.push(`/master/${dashString(master.name)}`);
+    } catch (error) {
+      // eslint-disable-next-line no-alert
+      alert('Sorry, try again later!');
+    }
+    setLoading(false);
   }
 
   return (
@@ -25,7 +35,12 @@ function Home({ chooseMaster }: Props) {
         <span className={Style.challenge}>Frontend challenge</span>
       </div>
       <div className={Style.buttonWrap}>
-        <button onClick={handleStart} className={Style.button} type="button">
+        <button
+          onClick={handleStart}
+          className={Style.button}
+          type="button"
+          disabled={loading}
+        >
           START
         </button>
       </div>
