e98871c67efe6c40fd7a9bb04941df76d01ecc41
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-10-24 19:01:54 -0300

    feat: add dependency injection on master page + choose again

--- src/presentation/pages/master/Master.tsx
+++ src/presentation/pages/master/Master.tsx
@@ -1,22 +1,43 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import classnames from 'classnames';
 import Style from './Master.module.scss';
 import DarthVader from './images/darth-vader.png';
 import LukeSkywalker from './images/luke-skywalker.png';
 import { BackButton, Header } from '../../components';
-import { undashString } from '../../../utils/StringUtils';
+import { dashString, undashString } from '../../../utils/StringUtils';
+import { ChooseMaster } from '../../../domain/usecases/ChooseMaster';
+import Masters from '../../../domain/data/Masters.json';
 
 type Params = {
   master: string;
 };
 
-function Master() {
+type Props = {
+  chooseMaster: ChooseMaster;
+};
+
+function Master({ chooseMaster }: Props) {
   const history = useHistory();
+  const [loading, setLoading] = useState<boolean>(false);
   const { master } = useParams<Params>();
 
   function handleBack() {
-    history.goBack();
+    history.replace('/');
+  }
+
+  async function handleChooseAgain() {
+    setLoading(true);
+    let newMmaster;
+    try {
+      newMmaster = await chooseMaster.choose(Masters);
+      setLoading(false);
+      history.push(`/master/${dashString(newMmaster.name)}`);
+    } catch (error) {
+      // eslint-disable-next-line no-alert
+      alert('Sorry, try again later!');
+      setLoading(false);
+    }
   }
 
   const masterClasses = classnames(Style.master, {
@@ -40,7 +61,12 @@ function Master() {
           <b>{undashString(master)}</b>
         </span>
         <div className={Style.buttonWrap}>
-          <button type="button" className={Style.button}>
+          <button
+            type="button"
+            className={Style.button}
+            disabled={loading}
+            onClick={handleChooseAgain}
+          >
             <span>choose your path again, Padawan</span>
           </button>
         </div>
