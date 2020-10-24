6fb5894730172bbe4424826207307158135aa915
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-10-24 15:39:11 -0300

    refactor: type responses

--- src/data/protocols/HttpClient.ts
+++ src/data/protocols/HttpClient.ts
@@ -3,5 +3,14 @@ export type HttpRequest = {
 };
 
 export interface HttpClient {
-  get: (params: HttpRequest) => Promise<any>;
+  get: (params: HttpRequest) => Promise<HttpResponse>;
 }
+
+export enum HttpStatusCode {
+  ok = 200,
+}
+
+export type HttpResponse = {
+  statusCode: HttpStatusCode;
+  body?: any;
+};
