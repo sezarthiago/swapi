28e0cbd0d476e30e623b3647ebfe46f1fb2bcf2d
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-10-24 15:40:03 -0300

    feat: improving httpClient and remote choose master

--- src/data/usecases/RemoteChooseMaster.ts
+++ src/data/usecases/RemoteChooseMaster.ts
@@ -1,16 +1,20 @@
 import { Master } from '../../domain/models/Master';
 import { ChooseMaster } from '../../domain/usecases/ChooseMaster';
-import { HttpClient } from '../protocols/HttpClient';
+import { HttpClient, HttpStatusCode } from '../protocols/HttpClient';
 
 export default class RemoteChooseMaster implements ChooseMaster {
   constructor(readonly httpClient: HttpClient, readonly url: string) {}
 
-  choose(masters: Master[]): Promise<Master> {
+  async choose(masters: Master[]): Promise<Master> {
     const promises = masters.map((master) =>
       this.httpClient.get({
         url: `${this.url}/${master.id}`,
       })
     );
-    return Promise.race(promises);
+    const result = await Promise.race(promises);
+    if (result.statusCode === HttpStatusCode.ok) {
+      return result.body;
+    }
+    throw new Error();
   }
 }
--- src/infra/http/AxiosHttpClient.ts
+++ src/infra/http/AxiosHttpClient.ts
@@ -1,8 +1,21 @@
-import Axios from 'axios';
-import { HttpClient, HttpRequest } from '../../data/protocols/HttpClient';
+import Axios, { AxiosResponse } from 'axios';
+import {
+  HttpClient,
+  HttpRequest,
+  HttpResponse,
+} from '../../data/protocols/HttpClient';
 
 export default class AxiosHttpClient implements HttpClient {
-  get(params: HttpRequest): Promise<any> {
-    return Axios.get(params.url);
+  async get(params: HttpRequest): Promise<HttpResponse> {
+    let response: AxiosResponse;
+    try {
+      response = await Axios.get(params.url);
+    } catch (error) {
+      response = error.response;
+    }
+    return {
+      statusCode: response.status,
+      body: response.data,
+    };
   }
 }
