2eaa53f989f022f823dc757a392441bcb8e0514b
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-10-24 18:50:39 -0300

    feat: add prop dark on back-icon

--- src/presentation/components/back-icon/BackIcon.tsx
+++ src/presentation/components/back-icon/BackIcon.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-function BackIcon() {
+type Props = {
+  dark: boolean;
+};
+
+function BackIcon({ dark = false }: Props) {
+  const color = dark ? '#2a2a2a' : 'white';
   return (
     <svg
       width="31"
@@ -11,7 +16,7 @@ function BackIcon() {
     >
       <path
         d="M31.0001 11.3333H6.69006L15.5117 2.51166L13.1551 0.154999L0.310059 13L13.1551 25.845L15.5117 23.4883L6.69006 14.6667H31.0001V11.3333Z"
-        fill="white"
+        fill={color}
       />
     </svg>
   );
