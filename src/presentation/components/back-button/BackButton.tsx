ea708f2a39e54a15aab60fb52855c328c0ecb235
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-10-24 18:50:19 -0300

    feat: add prop dark on back-button

--- src/presentation/components/back-button/BackButton.tsx
+++ src/presentation/components/back-button/BackButton.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import classnames from 'classnames';
 import BackIcon from '../back-icon/BackIcon';
 import Style from './BackButton.module.scss';
 
 type Props = {
   onClick: () => void;
+  dark: boolean;
 };
 
-function BackButton({ onClick }: Props) {
+function BackButton({ onClick, dark = false }: Props) {
+  const classes = classnames(Style.backButton, {
+    [Style.dark]: dark,
+  });
   return (
-    <button type="button" className={Style.backButton} onClick={onClick}>
-      <BackIcon />
+    <button type="button" className={classes} onClick={onClick}>
+      <BackIcon dark={dark} />
       <span>back</span>
     </button>
   );
