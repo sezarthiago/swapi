51e52c32a0c31448d4ef1ad9bccda4a0d0f45826
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-10-24 17:22:23 -0300

    refactor: move back-icon to its own file

--- src/presentation/components/back-button/BackButton.tsx
+++ src/presentation/components/back-button/BackButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BackIcon from '../back-icon/BackIcon';
 import Style from './BackButton.module.scss';
 
 type Props = {
@@ -8,18 +9,7 @@ type Props = {
 function BackButton({ onClick }: Props) {
   return (
     <button type="button" className={Style.backButton} onClick={onClick}>
-      <svg
-        width="31"
-        height="26"
-        viewBox="0 0 31 26"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M31.0001 11.3333H6.69006L15.5117 2.51166L13.1551 0.154999L0.310059 13L13.1551 25.845L15.5117 23.4883L6.69006 14.6667H31.0001V11.3333Z"
-          fill="white"
-        />
-      </svg>
+      <BackIcon />
       <span>back</span>
     </button>
   );
