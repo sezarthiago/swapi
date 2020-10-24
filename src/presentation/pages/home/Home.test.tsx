49f4bf1c473f8ebc77a5728f0d636cbd77f6a1a7
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-10-24 14:52:58 -0300

    test: add Home page

---
+++ src/presentation/pages/home/Home.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from './Home';
+
+describe('Home', () => {
+  test('spapshot', () => {
+    const sut = render(<Home />);
+    expect(sut).toMatchSnapshot();
+  });
+});
